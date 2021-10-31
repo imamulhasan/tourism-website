02fa8e9fac7f86600be7836de10998e488461751
unknown <imamulhasanturzo@gmail.com>
2021-10-31 04:34:33 -0700

    project complete

--- src/Pages/PrivateRoute/PrivateRoute.js
+++ src/Pages/PrivateRoute/PrivateRoute.js
@@ -6,9 +6,7 @@ const PrivateRoute = ({children, ...rest}) => {
     const {user,isLoading}=useAuth()
     if(isLoading){
        return  <div style={{height:"77vh",backgroundColor:'#DAEBE8'}} className="d-flex justify-content-center py-5">
-                    <span>Loading...</span>
-                    <div className="spinner-border" role="status">
-                    </div>
+                    
                 </div>
     }
     return (
